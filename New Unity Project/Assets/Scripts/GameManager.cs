8b86b605ad470e33ae221a3e0032f2d6ad3fb79b
wcyang8 <88764490+wcyang8@users.noreply.github.com>
2021-11-18 03:21:05 +0900

    닷지 플레이어 1.1
    닷지 플레이어 1.1

---
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    public GameObject level;
+    public GameObject bulletSpawnerPrefab;
+    private Vector3[] bulletSpawners = new Vector3[4];
+    int spawnCounter = 0;
+
+    private float surviveTime;
+    private bool isGameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        surviveTime = 0;
+        isGameover = false;
+
+        bulletSpawners[0].x = -8f;
+        bulletSpawners[0].y = 1f;
+        bulletSpawners[0].z = 8f;
+
+        bulletSpawners[1].x = 8f;
+        bulletSpawners[1].y = 1f;
+        bulletSpawners[1].z = 8f;
+
+        bulletSpawners[2].x = 8f;
+        bulletSpawners[2].y = 1f;
+        bulletSpawners[2].z = -8f;
+
+        bulletSpawners[3].x = -8f;
+        bulletSpawners[3].y = 1f;
+        bulletSpawners[3].z = -8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isGameover)
+        {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time : " + (int)surviveTime;
+
+            if(surviveTime < 5f && spawnCounter == 0)
+            {
+                GameObject bulletSpawner = Instantiate(bulletSpawnerPrefab, bulletSpawners[spawnCounter], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnCounter];
+                spawnCounter++;
+            }
+            if (surviveTime >= 5f && surviveTime < 10f && spawnCounter == 1)
+            {
+                GameObject bulletSpawner = Instantiate(bulletSpawnerPrefab, bulletSpawners[spawnCounter], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnCounter];
+                spawnCounter++;
+            }
+            if (surviveTime >= 10f && surviveTime < 15f && spawnCounter == 2)
+            {
+                GameObject bulletSpawner = Instantiate(bulletSpawnerPrefab, bulletSpawners[spawnCounter], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnCounter];
+                spawnCounter++;
+            }
+            if (surviveTime >= 15f && surviveTime < 20f && spawnCounter == 3)
+            {
+                GameObject bulletSpawner = Instantiate(bulletSpawnerPrefab, bulletSpawners[spawnCounter], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnCounter];
+                spawnCounter++;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        isGameover = true;
+        gameoverText.SetActive(true);
+
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        if(surviveTime > bestTime)
+        {
+            bestTime = surviveTime;
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+        recordText.text = "Best Time : " + (int)bestTime;
+    }
+}
